a02266e085449d6afc7a6f5f1cc2bcb309b614fe
Mahekaru <Mahekaru@gmail.com>
2022-08-12 14:33:19 -0400

    Adding Current .Net5 Project I had locally

---
+++ WebAPICore5/Classes/ProductQueryParams.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPICore5.Classes
+{
+    public class ProductQueryParams : QueryParams
+    {
+        public string Sku { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        public string Name { get; set; }
+        public string SearchTerm { get; set; }
+    }
+}
---
+++ WebAPICore5/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebAPICore5.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; } = false;
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+
+    }
+}
